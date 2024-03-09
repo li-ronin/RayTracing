1168c01c4d1e5a42552b3ac89c1694912496e2af
li-ronin <bkszdbhmm@qq.com>
2024-03-09 18:25:37 +0800

    the next week

--- 00Test/Run.bat
+++
@@ -1,6 +0,0 @@
-cd .\build\
-cmake -G "MinGW Makefiles" -S .. -B . -DCMAKE_C_COMPILER=E:/ENV/mingw64/bin/gcc.exe -DCMAKE_CXX_COMPILER=E:/ENV/mingw64/bin/g++.exe
-make
-cd ..
-.\bin\00Test.exe > img1.ppm
-
